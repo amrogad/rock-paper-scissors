ff25cbd757bf4b6b8775433cdf6499ef76ac8caf
Amro Gad <amrelsayedrabie@gmail.com>
2024-11-07 00:40:15 +0200

    created game.js and html files added and tested getComputerChoice function

---
+++ game.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  // Math.random(); generates a number between 0 and 1 for example: 0.33453452
+  // We multiply that number by 3 so we can get a value of 0.345743, 1.34536 or 2.342576
+  // if we round that value using Math.floor(); we get a 0, 1 or 2
+  // The reason it works is because 0.9 * 3 is 2.7 rounded to 3 and 0.1 or * 3 is 0.3 rounded to 0
+  let computerChoice = Math.floor(Math.random() * 3);
+  switch (computerChoice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
